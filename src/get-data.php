6bb48b94e7f05d4cde366cb265a5328b6e8ed86d
eunaosouodouglas <138947031+eunaosouodouglas@users.noreply.github.com>
2023-07-14 02:01:18 +0000

    Fixing video title characters

--- src/get-data.php
+++ src/get-data.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+
+    format_id:
+    247+251 = 
+    244+251 = 
+    302+251 = 
+
+*/
+
 // Importa o arquivo com as configurações de banco de dados e constantes.
 require('config.php');
 
@@ -7,15 +16,22 @@ require('config.php');
 $url = $_POST['url'];
 
 // Comando de Shell Script que será executado pelo interpretador do PHP.
-$command = YT_DLP.' '.$url.' --print "%(channel)s - %(title)s - %(upload_date)s - %(view_count)s - %(like_count)s - %(dislike_count)s - %(comment_count)s - %(was_live)s - %(duration>%H:%M:%S)s"';
+$command = YT_DLP.' '.$url.' --simulate --no-download-archive --skip-download --print "######%(id)s|||%(channel)s|||%(title)s|||%(upload_date)s|||%(view_count)s|||%(like_count)s|||%(dislike_count)s|||%(comment_count)s|||%(was_live)s|||%(duration)s|||%(format_id)s|||%(webpage_url)s######"';
 
-// Executa, trata e recebe o output em um array inteiro.
-// shell_exec = função nativa para executar o comando de shell.
-// trim = função nativa para remover espaços ou caracteres indesejados inseridos pelo PHP.
-// explode = função nativa para dividir o retorno em arrays, utilizando string ' - ' para delimitar.
-$output = explode(' - ', trim(shell_exec($command)));
+// shell_exec = executar o comando de shell retornando o output inteiro.
+// trim = remover espaços ou caracteres indesejados inseridos pelo PHP.
+// explode = dividir o retorno em arrays, utilizando string '######' e '|||' para delimitar.
+// array_map = fazer um loop em cada item de um array executando uma função de callback.
+// array_filter = remover índices nulos ou vazios.
+$output = shell_exec(trim($command));
+$video_listing = explode('######', $output);
+$space_removed = array_map('trim', $video_listing);
+$videos = array_filter($space_removed);
 
-// Exibe o retorno dos dados no navegador.
-echo "<pre>";
-var_dump($output);
-echo "</pre>";
+foreach($videos as $video) {
+    $arr = explode('|||', $video);
+    $map = array_map('trim', $arr);
+    echo "<pre>";
+    var_dump($map);
+    echo "</pre>";
+}
